13cc2fc78d56f1bc363ad06257970683f9e04040
hudahamid <huda.00091581@gmail.com>
2023-01-16 11:55:00 +0300

    ok

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+// code your solution here
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" },
+    //...
+  ];
+  
+  const superbowlWin = (collection) => {
+    for (const element of collection) {
+      if (element .result === "W") {
+        return element.year;
+      }
+    }
+  };
